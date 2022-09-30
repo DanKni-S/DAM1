ca9c31f8b9e7ddae000ffa78de4f03ff26da57cc
DanKni <dandmodan@hotmail.com>
2022-09-30 17:17:18 +0200

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Actividades/Ejercicio2.java
+++ src/Actividades/Ejercicio2.java
@@ -3,11 +3,18 @@ package Actividades;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("numeroA, numeroB, numeroC");
-		int numeroA = 2;
-		int numeroB = 5;
-		int numeroC = 10;
+		System.out.println("Numeros Alineados por la Derecha");
+			int NumeroA, NumeroB, NumeroC;
+			NumeroA = 1;
+			NumeroB = 840;
+			NumeroC = 63536;
+			System.out.printf("%5d\n", NumeroA);
+			System.out.printf("%5d\n", NumeroB);
+			System.out.printf("%d\n", NumeroC);
+		System.out.println("Numeros Alineados por la Izquierda");
+			System.out.printf("%d\n", NumeroA);
+			System.out.printf("%d\n", NumeroB);
+			System.out.printf("%d\n", NumeroC);
 	}
 
 }
