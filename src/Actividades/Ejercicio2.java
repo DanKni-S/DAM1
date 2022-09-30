47c814803aa97041d55dd0dae1c0844118d598b8
DanKni <dandmodan@hotmail.com>
2022-09-30 16:13:26 +0200

    Lo conseguiste Signed-off-by: DanKni <dandmodan@hotmail.com>

---
+++ src/Actividades/Ejercicio2.java
@@ -0,0 +1,13 @@
+package Actividades;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("numeroA, numeroB, numeroC");
+		int numeroA = 2;
+		int numeroB = 5;
+		int numeroC = 10;
+	}
+
+}
