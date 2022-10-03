c77560022d3ce190e1088e68951c243032ae9d41
DanKni <dandmodan@hotmail.com>
2022-10-03 16:40:48 +0200

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/UD01/Ejemplo3.java
+++ src/UD01/Ejemplo3.java
@@ -1,5 +1,16 @@
 package UD01;
 
+import java.util.Scanner;
+
 public class Ejemplo3 {
+
+	public static void main(String[] args) {
+		// 
+		Scanner teclado = new Scanner(System.in);
+		int dato;
+		System.out.println("Dime un número: ");
+		dato = teclado.nextInt();
+		System.out.println("El número que has escrito es: " + dato);
+	}
 
 }
