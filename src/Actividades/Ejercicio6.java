042a5439b11d617f57580eb2a0534253bf703c30
DanKni <dandmodan@hotmail.com>
2022-10-04 19:36:57 +0200

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Actividades/Ejercicio6.java
+++ src/Actividades/Ejercicio6.java
@@ -1,10 +1,21 @@
 package Actividades;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//
+		double nchicos, nchicas;
+		System.out.println("Introduce numero de chicos");
+		Scanner chicos = new Scanner(System.in);
+		nchicos = chicos.nextInt();
+		System.out.println("Introduce numero de chicas");
+		Scanner chicas = new Scanner(System.in);
+		nchicas = chicas.nextInt();
 
+		System.out.printf("Porcentaje de chicos: %.2f\n", (nchicos * 100) / (nchicos + nchicas));
+		System.out.printf("Porcentaje de chicas: %.2f", (nchicas * 100) / (nchicos + nchicas));
 	}
 
 }
---
+++ src/Actividades/Ejercicio7.java
@@ -0,0 +1,18 @@
+package Actividades;
+import java.util.Scanner;
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		// 
+		double nradio, naltura, pi = 3.14159265359;
+		System.out.println("Introduce cantidad de radio: ");
+		Scanner radio = new Scanner(System.in);
+		nradio = radio.nextDouble();
+		System.out.println("Introduce cantidad de altura: ");
+		Scanner altura = new Scanner(System.in);
+		naltura = altura.nextDouble();
+		
+		System.out.printf("Volumen: %.3f", pi*(nradio*nradio)*naltura);
+	}
+
+}
