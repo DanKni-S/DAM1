0049ac9f4c904dd2e72f817e1cd10e249e0e9295
DanKni <dandmodan@hotmail.com>
2022-10-05 19:12:13 +0200

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Actividades/Ejercicio3x2.java
+++ src/Actividades/Ejercicio3x2.java
@@ -1,10 +1,24 @@
 package Actividades;
 
+import java.util.Scanner;
+
 public class Ejercicio3x2 {
 
 	public static void main(String[] args) {
-		// 
-		
+		//
+		int mes;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("¿Mes?");
+		mes = teclado.nextInt();
+		if (mes < 1 || mes > 12) {
+			System.out.println("El mes debe estar comprendido entre 1 y 12");
+		} else if (mes == 2) {
+			System.out.println("28 días");
+		} else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+			System.out.println("30 días");
+		} else {
+			System.out.println("31 días");
+		}
 	}
 
 }
