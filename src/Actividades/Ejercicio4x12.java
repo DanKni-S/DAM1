4c1d2cf0f961cabc8b413a03b50a42e0ad3b1db2
DanKni <dandmodan@hotmail.com>
2022-10-19 18:18:19 +0200

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Actividades/Ejercicio4x12.java
+++ src/Actividades/Ejercicio4x12.java
@@ -3,8 +3,14 @@ package Actividades;
 public class Ejercicio4x12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//
+		double gradosC, gradosF, gradosK;
+		System.out.println("Celsius Fahrenheit Kelvin");
+		for (gradosC = -20.0; gradosC <= 100.0; gradosC += 10) {
+			gradosF = gradosC * (9.0 / 5.0) + 32.0;
+			gradosK = gradosC + 273.15;
+			System.out.printf(" %6.2f     %6.2f %6.2f\n", gradosC, gradosF, gradosK);
+		}
 	}
 
 }
