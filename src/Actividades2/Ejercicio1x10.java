e7b46bc98038032525803c9dc5827cf04cada535
DanKni <dandmodan@hotmail.com>
2022-11-07 16:34:01 +0100

    Signed-off-by: DanKni <dandmodan@hotmail.com>

---
+++ src/Actividades2/Ejercicio1x10.java
@@ -0,0 +1,10 @@
+package Actividades2;
+
+public class Ejercicio1x10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Actividades2/Ejercicio1x8.java
+++ src/Actividades2/Ejercicio1x8.java
@@ -2,9 +2,36 @@ package Actividades2;
 
 public class Ejercicio1x8 {
 
-	public static void main(String[] args) {
-		// 
-		
+	public static boolean sonAmigos(int numA, int numB) {
+		int sumaA = 0, sumaB = 0;
+		for (int i = 1; i < numA; i++) {
+			if (numA % i == 0) {
+				sumaA += i;
+			}
+		}
+		for (int i = 1; i < numB; i++) {
+			if (numB % i == 0) {
+				sumaB += i;
+			}
+		}
+		if (numA == sumaB && numB == sumaA) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-}
+	public static void main(String[] args) {
+		//
+		final int LIMITE = 500;
+		int i, a;
+		for (i = 1 ; i <= LIMITE ; i++) {
+			for (a = 1 ; a <= LIMITE ; a++) {
+				if (sonAmigos(i, a)) {
+					System.out.println("Los números " + i + " y " + a + " son amigos.");
+				}
+			}
+		}
+		}
+	}
+
