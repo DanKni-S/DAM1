63121d0f5473d5c06b56259dc501f3921dd03534
DanKni <dandmodan@hotmail.com>
2022-11-11 16:34:52 +0100

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/UD01/Prueba.java
+++ src/UD01/Prueba.java
@@ -6,25 +6,24 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dinero, articulo;
+		int dinero, articulo = 1, suma = 0;
 		Scanner teclado = new Scanner(System.in);
-		
+
 		System.out.println("Introduzca el dinero que tiene disponible ");
 		dinero = teclado.nextInt();
-		
-		System.out.println("Introduzca el precio del articulo ");
-		articulo = teclado.nextInt();
-		
-		for (articulo = 0 ; articulo <= dinero ; articulo++) {
-			dinero = dinero - articulo;
-			if (articulo <= dinero) {
-				System.out.println("Introduzca el precio del articulo ");
-				articulo = teclado.nextInt();
-			}
-			else {
-				System.out.println("Sobrepasa el limite del dinero");
+		while (articulo >= 0) {
+			System.out.println("Introduzca el precio del articulo ");
+			articulo = teclado.nextInt();
+			if (articulo != -1) {
+				suma = suma + articulo;
+				if (suma > dinero) {
+					System.out.println("Sobrepasa el limite del dinero");
+					suma = suma - articulo;
+				}
+
 			}
 		}
+		System.out.println("Total: " + suma);
 
 	}
 
