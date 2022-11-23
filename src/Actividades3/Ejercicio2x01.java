0f1033da1a93e294457321a55e6c44d7f5b5f5e5
DanKni <dandmodan@hotmail.com>
2022-11-23 19:46:20 +0100

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Actividades3/Ejercicio2x01.java
+++ src/Actividades3/Ejercicio2x01.java
@@ -1,10 +1,112 @@
 package Actividades3;
 
+import entrada.Teclado;
+import Constructor.Cuenta;
+
 public class Ejercicio2x01 {
+	public static void opcionesMenu() {
+		System.out.println();
+		System.out.println("0) Para salir ");
+		System.out.println("1) Visualizar en consola las dos cuenta ");
+		System.out.println("2) Ingresar un importe en la cuenta 1 ");
+		System.out.println("3) Retirar un importe de la cuenta 1 ");
+		System.out.println("4) Ingresar un importe en la cuenta 2 ");
+		System.out.println("5) Retirar un importe de la cuenta 2 ");
+		System.out.println("6) Transferir un importe de la cuenta 1 a la cuenta 2 ");
+		System.out.println("7) Transferir un importe de la cuenta 2 a la cuenta 1 ");
+	}
 
 	public static void main(String[] args) {
 		
+		int opcion;
+		double importeIngresar, importeRetirar;
+//		Cuenta cuenta1 = null, cuenta2 = null;
 		
+		Cuenta cuenta1 = new Cuenta(1, "Francisco Gómez");
+		System.out.println("ha creado la cuenta bancaria 1 ");
+		Cuenta cuenta2 = new Cuenta(2, "Victoria Serrano");
+		System.out.println("ha creado la cuenta bancaria 2 ");
+		
+		do {
+			opcionesMenu();
+			opcion = Teclado.leerEntero("Opcion (0 - 7): ");
+			switch (opcion) {
+			
+			case 0:
+				break;
+			case 1:
+				System.out.println(cuenta1.obtenerEstado());
+				System.out.println(cuenta2.obtenerEstado());
+				break;
+			case 2:
+				importeIngresar = Teclado.leerReal("Ingresa un importe: ");
+				if (cuenta1.ingresar(importeIngresar)) {
+					System.out.println("Se ha ingresado un importe en la cuenta 1 ");
+				}
+				else {
+					System.out.println("Error al ingresar un importe en la cuenta 1 ");
+					System.out.println("El importe debe ser positivo ");
+				}
+				break;
+			case 3:
+				importeRetirar = Teclado.leerReal("Retire un importe: ");
+				if (cuenta1.retirar(importeRetirar)) {
+					System.out.println("Se ha retirardo un importe de la cuenta 1 ");
+				}
+				else {
+					System.out.println("Error al retirar un importe en la cuenta 1 ");
+					System.out.println("El importe debe ser positivo ");
+					System.out.println("El importe debe ser menor o igual que el saldo de cuenta 1 ");
+				}
+				break;
+			case 4:
+				importeIngresar = Teclado.leerReal("Ingresa un importe: ");
+				if (cuenta2.ingresar(importeIngresar)) {
+					System.out.println("Se ha ingresado un importe en la cuenta 2 ");
+				}
+				else {
+					System.out.println("Error al ingresar un importe en la cuenta 2 ");
+					System.out.println("El importe debe ser positivo ");
+				}
+				break;
+			case 5:
+				importeRetirar = Teclado.leerReal("Retire un importe: ");
+				if (cuenta2.retirar(importeRetirar)) {
+					System.out.println("Se ha retirardo un importe de la cuenta 2 ");
+				}
+				else {
+					System.out.println("Error al retirar un importe en la cuenta 2 ");
+					System.out.println("El importe debe ser positivo ");
+					System.out.println("El importe debe ser menor o igual que el saldo de cuenta 2 ");
+				}
+				break;
+			case 6:
+				importeRetirar = Teclado.leerReal("¿Importe?: ");
+				if(cuenta1.tranferir(importeRetirar, cuenta2)) {
+					System.out.println("Se ha transferido un importe de la cuenta 1 a la cuenta 2 ");
+				}
+				else {
+					System.out.println("Error al retirar un importe en la cuenta 1 ");
+					System.out.println("El importe debe ser positivo ");
+					System.out.println("El importe debe ser menor o igual que el saldo de la cuenta 1 ");
+				}
+				break;
+			case 7:
+				importeRetirar = Teclado.leerReal("¿Importe?: ");
+				if(cuenta2.tranferir(importeRetirar, cuenta1)) {
+					System.out.println("Se ha transferido un importe de la cuenta 2 a la cuenta 1 ");
+				}
+				else {
+					System.out.println("Error al retirar un importe en la cuenta 2 ");
+					System.out.println("El importe debe ser positivo ");
+					System.out.println("El importe debe ser menor o igual que el saldo de la cuenta 2 ");
+				}
+				break;
+			}
+			
+		}
+		while(opcion != 0);
+		System.out.println("Programa finalizado sin errores. ");
 
 	}
 
--- src/Constructor/Cuenta.java
+++ src/Constructor/Cuenta.java
@@ -3,13 +3,59 @@ package Constructor;
 public class Cuenta {
 	
 	private int numero;
+	String cliente;
 	private double saldo;
 	
-	public Cuenta(int numero, double saldo) {
+//	Codifica una clase Cuenta que incluya los datos y las operaciones siguientes:
+//	Información de una cuenta: número, cliente y saldo.
+//	Un constructor que reciba como parámetros un número y un cliente y que indique por defecto que el saldo es 0.
+	
+	public Cuenta(int numero, String cliente) {
 		this.numero = numero;
-		this.saldo = saldo;
+		this.cliente = cliente;
+		this.saldo = 0.0;
 	}
+//	Un método de objeto obtenerEstado que devuelva el estado de la cuenta como cadena de caracteres, indicando el saldo con 2 dígitos decimales.
 	public String obtenerEstado() {
-		return "Cuenta [Número = " + numero + ", saldo " + String.format("%.2f", saldo);
+		return "CuentaBancaria [Número = " + numero + ", Cliente = " + cliente + ", saldo " + String.format("%.2f", saldo) + "]";
+	}
+//	Un método de objeto ingresar que tenga como parámetro un importe a ingresar en la cuenta, 
+//	que realice la operación cuando este importe sea positivo, y que devuelva un booleano indicando si el
+//	ingreso se ha realizado con éxito o no.
+	
+	public boolean ingresar(double importeIngresar) {
+		boolean ingresar = false;
+		if (importeIngresar > 0) {
+			this.saldo += importeIngresar;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+//	Un método de objeto retirar que tenga como parámetro un importe a retirar de la cuenta, que realice la operación cuando este importe
+//	sea positivo y sea menor o igual que el saldo, y que devuelva un booleano indicando si la retirada se ha realizado con éxito o no.
+	
+	public boolean retirar(double importeRetirar) {
+		if(importeRetirar > 0 && importeRetirar <= saldo) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+//	Un método de objeto transferir que tenga como parámetros un importe a transferir y una cuenta destino, 
+//	que realice la operación cuando este importe sea positivo y sea menor o igual que el saldo de la cuenta origen,
+//	y que devuelva un booleano indicando si la transferencia se ha realizado con éxito o no.
+	
+	public boolean tranferir(double importeTransferir, Cuenta cuentaDestino) {
+		if (importeTransferir > 0 && importeTransferir <= saldo) {
+			saldo -= importeTransferir;
+			cuentaDestino.saldo += importeTransferir;
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
