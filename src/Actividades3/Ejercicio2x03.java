72cfdf365f9b9428bc1be487b0f2393264c582db
DanKni <dandmodan@hotmail.com>
2022-11-30 18:49:26 +0100

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Actividades3/Ejercicio2x03.java
+++ src/Actividades3/Ejercicio2x03.java
@@ -13,11 +13,12 @@ public class Ejercicio2x03 {
 		System.out.println("4) Restar al tiempo otro tiempo ");
 		System.out.println();
 	}
+
 	public static Tiempo leerTimeValido() {
 		int horas = Teclado.leerEntero("¿Horas? ");
 		int minutos = Teclado.leerEntero("¿minutos? ");
 		int segundos = Teclado.leerEntero("¿Segundos? ");
-		while(! Tiempo.esValido(horas, minutos, segundos)) {
+		while (!Tiempo.esValido(horas, minutos, segundos)) {
 			System.out.println("Los datos del tiempo no son válidos: ");
 			System.out.println("La hora debe estar comprendida entre 0 y 23 ");
 			System.out.println("Los minutos debe estar comprendidos entre 0 y 59 ");
@@ -35,12 +36,12 @@ public class Ejercicio2x03 {
 		int opcion, horas, minutos, segundos;
 		Tiempo tiempo = null;
 		Tiempo otroTime = null;
-		
+
 		do {
 			menuOpciones();
 			opcion = Teclado.leerEntero("¿Opciones (0 - 5)?");
 			switch (opcion) {
-			
+
 			case 0:
 				break;
 			case 1:
@@ -61,18 +62,46 @@ public class Ejercicio2x03 {
 				break;
 			case 2:
 				if (tiempo == null) {
-					System.out.println("No existe nigún tiempo válido ");
-				}
-				else {
+					System.out.println("No existe ningún tiempo válido ");
+				} else {
 					System.out.println(tiempo.obtenerEstado());
 				}
 				break;
 			case 3:
+				if (tiempo == null) {
+					System.out.println("No existe ningún tiempo válido ");
+				} else {
+					otroTime = leerTimeValido();
+					System.out.println("Se ha creado otro tiempo válido ");
+					System.out.println(tiempo.obtenerEstado() + " + " + otroTime.obtenerEstado());
+				}
+				if (tiempo.sumar(otroTime)) {
+					System.out.println(tiempo.obtenerEstado());
+				} else {
+					System.out.println("Error al sumar el tiempo más otro tiempo ");
+					System.out.println("La suma debe ser igual o inferior a 23:60:60 ");
+				}
 				break;
 			case 4:
+				if (tiempo == null) {
+					System.out.println("No existe ningún tiempo válido ");
+				} else {
+					otroTime = leerTimeValido();
+					System.out.println("Se ha creado otro tiempo válido ");
+					System.out.println(tiempo.obtenerEstado() + " - " + otroTime.obtenerEstado());
+				}
+				if (tiempo.restar(otroTime)) {
+					System.out.println(tiempo.obtenerEstado());
+				} else {
+					System.out.println("Error al restar el tiempo más otro tiempo ");
+					System.out.println("La resta debe ser igual o superior a 00:00:00 ");
+				}
+				break;
+			default:
+				System.out.println("La opción del menú debe estar comprendida entre (0 - 4) ");
 				break;
 			}
-		}while(opcion != 0);
+		} while (opcion != 0);
 
 	}
 
--- src/Constructor/Tiempo.java
+++ src/Constructor/Tiempo.java
@@ -1,22 +1,24 @@
 package Constructor;
 
 public class Tiempo {
-	
+
 	private int horas;
 	private int minutos;
 	private int segundos;
-	
+
 	public Tiempo(int horas, int minutos, int segundos) {
 		this.horas = horas;
 		this.minutos = minutos;
 		this.segundos = segundos;
 	}
+
 //	public Tiempo() {
 //		random aleatorio = new Random();
 //	}
 	public String obtenerEstado() {
 		return String.format("%02d : %02d : %02d", this.horas, this.minutos, this.segundos);
 	}
+
 	public boolean sumar(Tiempo otro) {
 		int segundos = this.segundos + otro.segundos;
 		int minutos = this.minutos + otro.minutos;
@@ -30,7 +32,7 @@ public class Tiempo {
 			minutos = minutos - 60;
 			horas++;
 		}
-		if(horas < 24) {
+		if (horas < 24) {
 			this.segundos = segundos;
 			this.minutos = minutos;
 			this.horas = horas;
@@ -38,13 +40,35 @@ public class Tiempo {
 		}
 		return calculoExito;
 	}
+
+	public boolean restar(Tiempo otro) {
+		int segundos = this.segundos - otro.segundos;
+		int minutos = this.minutos - otro.minutos;
+		int horas = this.horas - otro.horas;
+		boolean calculoExito = false;
+		if (segundos >= 0) {
+			segundos = segundos + 60;
+			minutos--;
+		}
+		if (minutos >= 0) {
+			minutos = minutos + 60;
+			horas--;
+		}
+		if (horas >= 0) {
+			this.segundos = segundos;
+			this.minutos = minutos;
+			this.horas = horas;
+			calculoExito = true;
+		}
+		return calculoExito;
+	}
+
 	public static boolean esValido(int horas, int minutos, int segundos) {
 		if (horas >= 0 && horas < 24 && minutos >= 0 && minutos < 59 && segundos >= 0 && segundos < 59) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
-		
+
 	}
 }
