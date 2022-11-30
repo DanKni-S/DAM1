67dd61dfc220c7b6a871eb525ba7943b3c0652c2
DanKni <dandmodan@hotmail.com>
2022-11-30 18:16:54 +0100

    Signed-off-by: DanKni <dandmodan@hotmail.com>

--- src/Constructor/Tiempo.java
+++ src/Constructor/Tiempo.java
@@ -18,7 +18,25 @@ public class Tiempo {
 		return String.format("%02d : %02d : %02d", this.horas, this.minutos, this.segundos);
 	}
 	public boolean sumar(Tiempo otro) {
-		
+		int segundos = this.segundos + otro.segundos;
+		int minutos = this.minutos + otro.minutos;
+		int horas = this.horas + otro.horas;
+		boolean calculoExito = false;
+		if (segundos >= 60) {
+			segundos = segundos - 60;
+			minutos++;
+		}
+		if (minutos >= 60) {
+			minutos = minutos - 60;
+			horas++;
+		}
+		if(horas < 24) {
+			this.segundos = segundos;
+			this.minutos = minutos;
+			this.horas = horas;
+			calculoExito = true;
+		}
+		return calculoExito;
 	}
 	public static boolean esValido(int horas, int minutos, int segundos) {
 		if (horas >= 0 && horas < 24 && minutos >= 0 && minutos < 59 && segundos >= 0 && segundos < 59) {
