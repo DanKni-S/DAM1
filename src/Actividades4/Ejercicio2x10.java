c24c26ebfd40fbff5a44864a1dd201baf99bfd9e
DanKni-S <71387990+DanKni-S@users.noreply.github.com>
2022-12-20 20:06:29 +0100

    Add files via upload

--- src/Actividades4/Ejercicio2x10.java
+++ src/Actividades4/Ejercicio2x10.java
@@ -22,12 +22,28 @@ public class Ejercicio2x10 {
 			System.err.println("La frase debe tener " + longtudMinima + " caracteres como mínimo ");
 			System.err.println("La frase debe tener " + longtudMaxima + " caracteres como máximo ");
 			frase = Teclado.leerCadena(mensaje);
+			longitud = frase.length();
 		}
 		return frase;
 	}
 
 	public static void main(String[] args) {
 		
+		String frase = leerFrase("¿Frase? ", 10, 60);
+		
+		frase = frase.trim();
+		while (frase.indexOf(" ") != -1) {
+			frase = frase.replace(" ", " ");
+		}
+		
+		int numeroEspacios = contarEspacio(frase);
+		if (frase.length() > 0) {
+			int numeroPalabras = numeroEspacios + 1;
+			System.out.println("La frase tiene " + numeroPalabras + " palabras. ");
+		}
+		else {
+			System.out.println("La frase no tiene palabras. ");
+		}
 
 	}
 
