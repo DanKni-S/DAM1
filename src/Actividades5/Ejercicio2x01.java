16f6401dec88912660ff8ed31121973ad93118f0
DanKni-S <71387990+DanKni-S@users.noreply.github.com>
2023-01-11 19:34:08 +0100

    Add files via upload

---
+++ src/Actividades5/Ejercicio2x01.java
@@ -0,0 +1,16 @@
+package Actividades5;
+
+public class Ejercicio2x01 {
+
+	public static void main(String[] args) {
+		
+		int [][] matriz = new int [4][4];
+		for(int fila = 0 ; fila < matriz.length ; fila++) {
+			for(int columna = 0 ; columna < matriz[fila].length ; columna++) {
+				matriz[fila][columna] = 16;
+			}
+		}
+
+	}
+
+}
